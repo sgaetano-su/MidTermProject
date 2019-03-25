4293307d5e9bc46007b7769fe7fc52a3ed5700cc
Shawn Gaetano <sgaetano@syr.edu>
2019-03-25 05:12:28 -0400

    Small changes

--- variables.js
+++ variables.js
@@ -5,3 +5,9 @@ $(document).ready(function(){
 $(document).ready(function() {
   $('li.active').removeClass('active');
 });
+
+$(document).ready(function () {
+  $(".navbar-nav li.trigger-collapse a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
