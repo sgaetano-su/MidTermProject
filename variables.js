c92ad0e3ea64093428a01af3ff9b842d2c14a2cb
Shawn Gaetano <sgaetano@syr.edu>
2019-03-25 06:03:02 -0400

    Final fixes

--- variables.js
+++ variables.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-    $("#myModal2").modal('show');
+  $("#myModal2").modal('show');
 });
 
 $(document).ready(function() {
-  $('li.active').removeClass('active');
+$('li.active').removeClass('active');
 });
 
-
 $(document).ready(function () {
- $(".navbar-nav li.trigger-collapse a").click(function(event) {
-   $(".navbar-collapse").collapse('hide');
- });
+$(".navbar-nav li.trigger-collapse a").click(function(event) {
+  $(".navbar-collapse").collapse('hide');
+});
+});
